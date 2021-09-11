fc4e2638f39f53ab344f62c2d26b17ff369cf590
Brian <brianposs05@gmail.com>
2021-09-11 18:09:27 +0200

    Quicksort verändert

--- Sortieren_Brian.java
+++ Sortieren_Brian.java
@@ -6,116 +6,159 @@ import java.util.Random;
  * Hierfuer stehen drei unterschiedliche Verfahren zur Verfuegung: Bubblesort, Selectionsort und Insertionsort
  * Fuer die Erzeugung von Zufallszahlen steht die Klasse Random zur Verfügung, die zuvor siehe 1. Zeile importiert 
  * werden muss.
- * 
+ *
  * @author Sebastian Horn
  * @version 0.2
  */
 
 public class Sortieren_Brian
-{            
-    
+{
+
     //Deklaration der globalen Variablen/Zustandvariablen
     private int zZahlenarray[];
     private Random zZufallsgenerator;
-            
-    //Methoden/ Eigenschaften von Objekten der klasse Sortieren 
-    
+
+    //Methoden/ Eigenschaften von Objekten der klasse Sortieren
+
     /**
      * Konstruktor der Klasse Sortieren. Im Konstruktor werden die zuvor deklarieten Zustandsvariablen
-     * erzeugt. Nach dem Aufruf des Konstruktors ist ein Objekt der Klasse Sortieren erzeugt worden. Ein Array 
-     * mit dem Namen zZahlenarray und der Grösse 30 wurde erzeugt. Des Weiteren wurde ein Objekt der Klasse Random 
+     * erzeugt. Nach dem Aufruf des Konstruktors ist ein Objekt der Klasse Sortieren erzeugt worden. Ein Array
+     * mit dem Namen zZahlenarray und der Grösse 30 wurde erzeugt. Des Weiteren wurde ein Objekt der Klasse Random
      * zur Erzeugung von Zufallszahlen erzeugt.
      * Es kann mehrer Konstruktoren geben, diese unterscheiden sich in den/ dem Parameter(n).
      */
     public Sortieren_Brian()
-    {  
+    {
         //Zustandvariablen werden initialisiert
         zZahlenarray = new int [30];
-        zZufallsgenerator = new Random(); 
+        zZufallsgenerator = new Random();
     }
-    
+
     /**
      * 2. Konstruktor der Klasse Sortieren. Im Konstruktor werden die zuvor deklarieten Zustandsvariablen
-     * erzeugt. Nach dem Aufruf des Konstruktors ist ein Objekt der Klasse Sortieren erzeugt worden. Ein Array 
-     * mit dem Namen zZahlenarray und einer variablen Groesse wurde erzeugt. Des Weiteren wurde ein Objekt der Klasse Random 
+     * erzeugt. Nach dem Aufruf des Konstruktors ist ein Objekt der Klasse Sortieren erzeugt worden. Ein Array
+     * mit dem Namen zZahlenarray und einer variablen Groesse wurde erzeugt. Des Weiteren wurde ein Objekt der Klasse Random
      * zur Erzeugung von Zufallszahlen erzeugt.
      * Es kann mehrer Konstruktoren geben, diese unterscheiden sich in den/ dem Parameter(n).
      */
     public Sortieren_Brian(int pGroesse)
-    {  
+    {
         //Zustandvariablen werden initialisiert
         zZahlenarray = new int [pGroesse];
-        zZufallsgenerator = new Random(); 
+        zZufallsgenerator = new Random();
     }
-    
+
     /**
      * 3. Konstruktor der Klasse Sortieren. Im Konstruktor werden die zuvor deklarieten Zustandsvariablen
-     * erzeugt. Nach dem Aufruf des Konstruktors ist ein Objekt der Klasse Sortieren erzeugt worden. Ein Array 
-     * mit dem Namen zZahlenarray und einer variablen Groesse wurde erzeugt. Des Weiteren wurde ein Objekt der Klasse Random 
+     * erzeugt. Nach dem Aufruf des Konstruktors ist ein Objekt der Klasse Sortieren erzeugt worden. Ein Array
+     * mit dem Namen zZahlenarray und einer variablen Groesse wurde erzeugt. Des Weiteren wurde ein Objekt der Klasse Random
      * zur Erzeugung von Zufallszahlen erzeugt und das Array mit Zufallszahlen im Zahlenspektrum von 0 - pMaxZahl befuellt.
      * Es kann mehrer Konstruktoren geben, diese unterscheiden sich in den/ dem Parameter(n).
-     * 
+     *
      * @param int pGroesse gibt die Groesse des Arrays an
      * @param int pMaxZahl gibt die groesste moegliche zu erzeugenden Zufallszahl an
      */
     public Sortieren_Brian(int pGroesse, int pMaxZahl)
-    {  
-        //Zustandvariablen werden initialisiert
-        //Dein Quellcode hier
+    {
+        this.zZahlenarray = new int [pGroesse];
+        zZufallsgenerator = new Random();
+        for(int i = 0; i<this.zZahlenarray.length; i++) this.zZahlenarray[i] = zZufallsgenerator.nextInt(pMaxZahl);
     }
-    
+
     /**
      * Methode bfmsZufall
-     * Auftrag: Nach dem Aufruf dieser Methode ist das Array zZahlenarray mit zufälligen Zahlen im Spektrum 
+     * Auftrag: Nach dem Aufruf dieser Methode ist das Array zZahlenarray mit zufälligen Zahlen im Spektrum
      * von 0 - 1000 befüllt
      */
     public void bfmsZufall()
-    {         
+    {
         //Mit Hilfe der Methode nextInt(int pInt) der Klasse Random kann eine Zufallszahl im Spektrum
-        // von 0 - pInt erzeugt werden Bs.p: der Aufruf zZufallsgenerator.nextInt(1000) erzeugt eine Zufallszahl im Spektrum 
+        // von 0 - pInt erzeugt werden Bs.p: der Aufruf zZufallsgenerator.nextInt(1000) erzeugt eine Zufallszahl im Spektrum
         // von 0 - 1000
     }
-    
+
     /**
      * Methode bfmsZufallflex
-     * Auftrag: Nach dem Aufruf dieser Methode ist das Array zZahlenarray mit zufälligen Zahlen im Spektrum 
+     * Auftrag: Nach dem Aufruf dieser Methode ist das Array zZahlenarray mit zufälligen Zahlen im Spektrum
      * von 0 - pMaxZahl befüllt.
      */
     public void bfmsZufallflex(int pMaxZahl)
     {
-       // Dein Quellcode hier
+        // Dein Quellcode hier
     }
 
 
     /**
      * Method bfms
-     * Auftrag: Nach dem Aufruf der Methode ist das Array zZahlenarray mit aufsteigenden Zahlenwerten 
+     * Auftrag: Nach dem Aufruf der Methode ist das Array zZahlenarray mit aufsteigenden Zahlenwerten
      * beginnend bei 1 und einer Schrittweite von 1 gefuellt.
      *
      */
     public void bfms()
-    { 
+    {
         for(int i = 0; i < 30; i++)
         {
             zZahlenarray[i]= i +1;
         }
     }
-    
-    
+
+
     /**
      * Method bubblesort
      * Auftrag: Nach Aufruf der Methode ist das Array zZahlenarray nach dem Prinzip Bubblesort sortiert worden.
-     * 
+     *
      * Eine ausführliche Beschreibung muss hier eurerseits erfolgen!!!!!! Hilfe zum Prinzip findest du unter:
      * https://www.youtube.com/watch?v=lyZQPjUT5B4 und im Buch!!!!!
-     * 
+     *
      * Hierfür muss das Array vorher mit Zahlenwerten gefüllt worden sein d.h. eine der Methoden zum Befüllen des Arrays
      * muss aufgerufen worden sein.
      * Wenn nicht bricht BlueJ mit einer Fehlermedung ab.
      */
     public void bubblesort()
     {   
-       //Dein Quellcode
+         // dieser loop geht einmal durch den array durch
+         for(int i = 0; i < zZahlenarray.length; i++){  
+             /**
+              * dieser loop geht immer bis zu dem array bis zu dem ende minus dem index,
+              * da nach einem durchgang die letzte zahl sortiert ist
+              * und - 1, da neben rechts der letzten zahl nichts ist
+              */
+             for(int j = 0; j < zZahlenarray.length - i - 1; j++){  
+                 // wir gucken ob die zahl rechts neben von dem aktuellen index kleiner ist
+                 if(zZahlenarray[j] > zZahlenarray[j + 1]){   
+                     // wir vertauschen die beiden zahlen
+                     int temp = zZahlenarray[j];  
+                     zZahlenarray[j] = zZahlenarray[j + 1];  
+                     zZahlenarray[j + 1] = temp;  
+                 }  
+             }  
+         }  
+    }
+
+    public void bubblesort_2()
+    {   
+        // dieser loop geht einmal durch den array durch
+        int i = 0;
+        boolean sort = false;
+        while(!sort){  
+             sort = true;
+             /**
+              * dieser loop geht immer bis zu dem array bis zu dem ende minus dem index,
+              * da nach einem durchgang die letzte zahl sortiert ist
+              * und - 1, da neben rechts der letzten zahl nichts ist
+              */
+             for(int j = 0; j < zZahlenarray.length - i - 1; j++){  
+                 // wir gucken ob die zahl rechts neben von dem aktuellen index kleiner ist
+                 if(zZahlenarray[j] > zZahlenarray[j + 1]){   
+                     // wir vertauschen die beiden zahlen
+                     int temp = zZahlenarray[j];  
+                     zZahlenarray[j] = zZahlenarray[j + 1];  
+                     zZahlenarray[j + 1] = temp;
+                     sort = false;
+                 }  
+             }  
+             i++;
+         }  
     }
     
     /**
@@ -132,9 +175,23 @@ public class Sortieren_Brian
     public void insertionsort()
     {   
         //Tipp: i = 1
-        for (int i=1; i<=zZahlenarray.length-1; i++)
+        for (int i = 1; i <= zZahlenarray.length - 1; i++)
         {   
-            
+            // wir gucken ob die links zahl größer ist als die rechte
+            if(zZahlenarray[i - 1] > zZahlenarray[i]) {
+                /**
+                 * das i > -1 sorgt dafür das die kleinste zahl des arrays nicht 
+                 * zu weit nach links geschoben wird ( out of bounds )
+                 * wir loopen so lange bis die linke zahl größer ist als die aktuelle
+                 */
+                while( (i > -1) && zZahlenarray[i - 1] > zZahlenarray[i]) {
+                    // wir vertauschen die beiden zahlen und gehen eins nach rechts
+                    int temp = zZahlenarray[i];  
+                    zZahlenarray[i] = zZahlenarray[i - 1];  
+                    zZahlenarray[i - 1] = temp;  
+                    i--;
+                }
+            }    
         }    
     }
     
@@ -151,10 +208,27 @@ public class Sortieren_Brian
      */
     public void selectionSort()
     {
-        //Tipp: Auch hier werden 2 Schleifen benötigt
+        // wir gehen durch den ganzen loop, - 1 weil die letzte zahl nichts rechts von ihr hat
+        for(int i = 0; i < zZahlenarray.length - 1; i++) { 
+            // wir deklarieren den index mit der niedrigsten zahl als die aktuelle zahl
+            int lowest = i;
+            // wir gehen durch den restlichen array und suchen immer einer niedrigere zahl
+            for(int j = i + 1; j < zZahlenarray.length; j++) {  
+                if(zZahlenarray[j] < zZahlenarray[lowest]){
+                    // deklarierung des index mit der (aktuellen) niedgristen zahl
+                    lowest = j;
+                }
+            }
+            
+            // wir vertauschen den aktuellen index mit dem der niedrigsten
+            int temp = zZahlenarray[i];
+            zZahlenarray[i] = zZahlenarray[lowest];  
+            zZahlenarray[lowest] = temp;
+        }
     }    
-    
-    
+
+
+
     /**
      * Methode lineareSuche
      *
@@ -162,73 +236,83 @@ public class Sortieren_Brian
      * @return true wenn die Zahl gefunden wurde, sonst false
      */
     public boolean lineareSuche(int pZahl)
-    {      
-       //
-       return false;
-    } 
-    
+    {
+        //
+        return false;
+    }
+
     /**
      * Initiale Sortiermethode fuer den Quicksort-Algorithmus
      */
     public void quickSort()
     {
+        System.out.print("Test\n");
         System.out.println("--------------------------------------------------------------------");
         System.out.println("Beginne neuen Sortiervorgang mit Quicksort");
         //zeigeElementenfolge();
-        quicksort_intern(0, zZahlenarray.length-1);        
+        quicksort_intern(0, zZahlenarray.length-1);
         //zeigeElementenfolge();
-        //System.out.println("--------------------------------------------------------------------");  
-    } 
-    
+        //System.out.println("--------------------------------------------------------------------");
+    }
+
     private void quicksort_intern(int pL, int pR)
     {
         int hLinks = pL;  // Hilfszeiger f¸r links
-        int hRechts = pR; // Hilfszeiger f¸r rechts
-        int hPivot = (int) (Math.random()*(pR-pL+1))+pL; // Stelle des Pivot
+        int hRechts = pR; // Hilfszeiger f¸r recht
         if (pL < pR)
         {
-          int pi = partition(zZahlenarray, pL, pR, hPivot); 
-          quicksort_intern(pL, pi-1);
-          quicksort_intern(pi+1, pR);
+            int pi = partition(pL, pR);
+            quicksort_intern(pL, pi-1);
+            quicksort_intern(pi+1, pR);
+            //for(int i = 0; i< this.zZahlenarray.length; i++)System.out.println(this.zZahlenarray[i]);
         }
     }
-    
-    public static void tauschen(int[] array, int j, int i)
+
+    public void tauschen( int j, int i)
     {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        int temp = this.zZahlenarray[i];
+        this.zZahlenarray[i] = this.zZahlenarray[j];
+        this.zZahlenarray[j] = temp;
     }
-    
+
     public static void main(String[] args){
-       Sortieren_Brian test = new Sortieren_Brian();
-       test.quickSort();
+        System.out.println("Beginne neuen Sortiervorgang mit Quicksort");
+        Sortieren_Brian test = new Sortieren_Brian(10, 10);
+        int[] array = test.zZahlenarray;
+        for(int i = 0; i< test.zZahlenarray.length; i++)System.out.println((int) test.zZahlenarray[i]);
+        test.quickSort(); 
+        for(int i = 0; i< test.zZahlenarray.length; i++)System.out.print( array[i]);
+        System.out.println("Sortiert: ");
+        for(int i = 0; i< test.zZahlenarray.length; i++)System.out.println((int) test.zZahlenarray[i]);
     }
-    
-    
-    
-    public static int partition(int[] array, int lowest,int highest,int hPivot)
+
+
+
+    public int partition( int lowest,int highest)
     {
+        int hPivot = (int) (Math.random()*(highest-lowest+1))+lowest; // Stelle des Pivot
         int teile = lowest - 1;
+        tauschen(hPivot, highest);
+        hPivot = this.zZahlenarray[highest];
         for(int suche = lowest;  suche < highest; suche++)
         {
-            if(array[suche] < hPivot)
+            if(this.zZahlenarray[suche] < hPivot)
             {
                 teile++;
-                tauschen(array, teile, suche);
+                tauschen(teile, suche);
             }
         }
-        
-        tauschen(array, teile+1, highest);
+
+        tauschen(teile+1, highest);
         return (teile+1);
     }
-    
+
     public void mergeSort()
     {
-        
+
     }
-    
-    
+
+
     private void merge_intern(int links, int mitte, int rechts)
     {
         int i,j,k;
@@ -265,16 +349,18 @@ public class Sortieren_Brian
             i++;
         }
     }
-    
+
     public boolean binaereSuche(int pGesuchteZahl)
     {
         boolean gefunden = binaereSuche_intern(pGesuchteZahl,0, zZahlenarray.length -1);
-        return gefunden;        
+        return gefunden;
     }
-    
+
     private boolean binaereSuche_intern(int pZahl, int pBeginn, int pEnde)
     {
         //Dein Quellcode hier
-      return true;  
+        return true;
     }
+    
+
 }
